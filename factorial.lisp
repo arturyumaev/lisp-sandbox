043ae7d29b3745e161921496675ee38250a0b73f
Artur Yumaev <art.yumaev@gmail.com>
2021-04-16 15:24:37 +0300

    Tabs

--- factorial.lisp
+++ factorial.lisp
@@ -1,7 +1,7 @@
 #!/usr/local/bin/sbcl --script
 
 (defun fac(n)
-	(if (<= n 1)
+  (if (<= n 1)
     1
     (* n (fac (- n 1)))
   )
@@ -9,19 +9,19 @@
 
 (defparameter TESTN 1)
 (defun compare(actual expected)
-	(format t "Test ~A. Result: ~A --- ~A~%"
-		TESTN
-		actual
-		(if (= actual expected) 'OK 'ERROR)
-	)
-	(setf TESTN (+ TESTN 1))
+  (format t "Test ~A. Result: ~A --- ~A~%"
+    TESTN
+    actual
+    (if (= actual expected) 'OK 'ERROR)
+  )
+  (setf TESTN (+ TESTN 1))
 )
 
 (defun test()
-	(compare (fac 0) 1)
-	(compare (fac 1) 1)
-	(compare (fac 5) 120)
-	(compare (fac 6) 720)
+  (compare (fac 0) 1)
+  (compare (fac 1) 1)
+  (compare (fac 5) 120)
+  (compare (fac 6) 720)
 )
 
 (test)
